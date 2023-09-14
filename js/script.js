3fb56d1ba4bac3f9618de249b53dc2d4be91f495
wawawiwaha <wawawiwaha@gmail.com>
2023-09-14 21:16:09 +0700

    update project revou

--- js/script.js
+++ js/script.js
@@ -1,228 +1,72 @@
-class Helper {
-  checkHitung(val) {
-    if (val.length <= 0) return false;
+function hitungLuas() {
+  // Mengambil nilai panjang sisi dari input
+  var inputLuas = document.getElementById("panjangLuasSisi");
+  var panjangSisi = inputLuas.value.trim();
 
-    for (let i = 0; i < val.length; i++) {
-      if (!(val[i] >= "0" && val[i] <= "9")) return false;
-    }
+  // Membuat pola regex untuk angka
+  var regex = /^[0-9]*$/;
 
-    let valueInt = parseInt(val);
-    if (valueInt <= 0) return false;
-
-    return true;
+  // Mengecek apakah input sesuai dengan pola regex
+  if (!regex.test(panjangSisi)) {
+      document.getElementById("errorLuas").innerHTML = "Masukkan jumlah sisi berupa angka!";
+      inputLuas.focus();
+      return;
   }
+
+  // Menghapus pesan validasi jika input valid
+  document.getElementById("errorLuas").innerHTML = "";
+
+  // Menghitung luas persegi
+  var luas = panjangSisi * panjangSisi;
+
+  // Menampilkan hasil di elemen dengan id "hasil"
+  document.getElementById("hasilLuas").innerHTML = "L = " + luas;
+  document.getElementById("luasSisi").innerHTML = "L = " + panjangSisi + " x " + panjangSisi;
+
 }
 
-class Type {
-  constructor(elementId, inputId, helper) {
-    this.elementId = elementId;
-    this.inputId = inputId;
-    this.helper = helper;
-  }
-
-  show() {
-    document.getElementById(this.elementId).style.display = "block";
-  }
-
-  hide() {
-    document.getElementById(this.elementId).style.display = "none";
-  }
-
-  reset() {
-    document.getElementById(this.inputId).value = "";
-
-    if (document.getElementById(this.elementId).firstElementChild) {
-      document
-        .getElementById(this.elementId)
-        .removeChild(document.getElementById(this.elementId).firstElementChild);
-    }
-    this.hide();
-  }
-
-  hitung() {
-    throw new Error("Method 'hitung()' harus diimplementasikan!");
-  }
-}
-
-class PersegiLuas extends Type {
-  constructor(elementId, inputId, helper) {
-    super(elementId, inputId, helper);
-  }
-
-  hitung() {
-    let value = document.getElementById(this.inputId).value;
-
-    if (!this.helper.checkHitung(value)) {
-      this.show();
-
-      document.getElementById(this.elementId).style.display = "block";
-
-      if (document.getElementById(this.elementId).firstElementChild) {
-        document
-          .getElementById(this.elementId)
-          .removeChild(
-            document.getElementById(this.elementId).firstElementChild
-          );
-      }
-
-      const res = document.createElement("p");
-      res.classList = "font-roboto font-red font-bold line-height-24";
-
-      res.innerText = `Masukan jumlah sisi Persegi!`;
-      document.getElementById(this.elementId).appendChild(res);
-    } else {
-      this.show();
-
-      document.getElementById(this.elementId).style.display = "block";
-
-      if (document.getElementById(this.elementId).firstElementChild) {
-        document
-          .getElementById(this.elementId)
-          .removeChild(
-            document.getElementById(this.elementId).firstElementChild
-          );
-      }
-
-      let valueInt = parseInt(value);
-      let hasil = valueInt * valueInt;
-
-      const res = document.createElement("p");
-      res.classList = "font-roboto line-height-24";
-
-      res.innerText = `L = S x S\nL = ${valueInt} x ${valueInt}\nL = ${hasil}`;
-      document.getElementById(this.elementId).appendChild(res);
-    }
-  }
-}
-
-class PersegiKeliling extends Type {
-  constructor(elementId, inputId, helper) {
-    super(elementId, inputId, helper);
-  }
-
-  hitung() {
-    let value = document.getElementById(this.inputId).value;
-
-    if (!this.helper.checkHitung(value)) {
-      this.show();
-
-      document.getElementById(this.elementId).style.display = "block";
-
-      if (document.getElementById(this.elementId).firstElementChild) {
-        document
-          .getElementById(this.elementId)
-          .removeChild(
-            document.getElementById(this.elementId).firstElementChild
-          );
-      }
-
-      const res = document.createElement("p");
-      res.classList = "font-roboto font-red font-bold line-height-24";
-
-      res.innerText = `Masukan jumlah sisi Persegi!`;
-      document.getElementById(this.elementId).appendChild(res);
-    } else {
-      this.show();
-
-      document.getElementById(this.elementId).style.display = "block";
-
-      if (document.getElementById(this.elementId).firstElementChild) {
-        document
-          .getElementById(this.elementId)
-          .removeChild(
-            document.getElementById(this.elementId).firstElementChild
-          );
-      }
-
-      let valueInt = parseInt(value);
-      let hasil = 4 * valueInt;
-
-      const res = document.createElement("p");
-      res.classList = "font-roboto line-height-24";
-
-      res.innerText = `K = 4 x S\nK = 4 x ${valueInt}\nK = ${hasil}`;
-      document.getElementById(this.elementId).appendChild(res);
-    }
-  }
+function resetLuas() {
+  // Mereset nilai input dan hasil
+  document.getElementById("panjangLuasSisi").value = "";
+  document.getElementById("hasilLuas").innerHTML = "";
+  document.getElementById("luasSisi").innerHTML = "";
+  document.getElementById("errorLuas").innerHTML = "";
 }
 
 
-class ButtonType {
-  constructor(elementId) {
-    this.elementId = elementId;
+
+function hitungKeliling() {
+  // Mengambil nilai panjang sisi dari input
+  var inputKeliling = document.getElementById("panjangKelilingSisi");
+  var panjangSisiKeliling = inputKeliling.value.trim();
+
+  // Membuat pola regex untuk angka
+  var regex = /^[0-9]*$/;
+
+  // Mengecek apakah input sesuai dengan pola regex
+  if (!regex.test(panjangSisiKeliling)) {
+      document.getElementById("errorKeliling").innerHTML = "Masukan jumlah sisi berupa angka!";
+      inputKeliling.focus();
+      return;
   }
 
-  activate() {
-    document.getElementById(this.elementId).style.color = "#fff";
-    document.getElementById(this.elementId).style.backgroundColor = "#4f6ede";
-    document.getElementById(this.elementId).style.borderColor = "#4f6ede";
-  }
+  // Menghapus pesan validasi jika input valid
+  document.getElementById("errorKeliling").innerHTML = "";
 
-  deactivate() {
-    document.getElementById(this.elementId).style.color = "#4f6ede";
-    document.getElementById(this.elementId).style.backgroundColor = "#fff";
-    document.getElementById(this.elementId).style.borderColor = "#4f6ede";
-  }
+  // Menghitung keliling persegi
+  var keliling = 4 * parseFloat(panjangSisiKeliling);
+
+  // Menampilkan hasil di elemen dengan id "hasilKeliling"
+  document.getElementById("hasilKeliling").innerHTML = "K = " + keliling;
+  document.getElementById("kelilingSisi").innerHTML = "K" + " " + "=" + " " + "4" + " " + "x" + " " + panjangSisiKeliling;
 }
 
-function getHitung(type) {
-  switch (type) {
-    case "persegiLuas": {
-      persegiLuas.hitung();
-      break;
-    }
-    case "persegiKeliling": {
-      persegiKeliling.hitung();
-      break;
-    }
-   
-  }
+function resetKeliling() {
+  // Mereset nilai input, hasil, dan pesan validasi
+  document.getElementById("panjangKelilingSisi").value = "";
+  document.getElementById("hasilKeliling").innerHTML = "";
+  document.getElementById("kelilingSisi").innerHTML = "";
+  document.getElementById("errorKeliling").innerHTML = "";
 }
 
-function resetHasil(type) {
-  switch (type) {
-    case "persegiLuas": {
-      persegiLuas.reset();
-      break;
-    }
-    case "persegiKeliling": {
-      persegiKeliling.reset();
-      break;
-    }
-  }
-}
-
-function btnTypeHandle(type) {
-  btnTypePersegi.deactivate();
-
-  document.getElementsByClassName("persegiContainer")[0].style.display = "none";
-
-  switch (type) {
-    case "persegi": {
-      document.getElementsByClassName("persegiContainer")[0].style.display =
-        "flex";
-      btnTypePersegi.activate();
-      break;
-    }
-  }
-}
-
-const helper = new Helper();
-
-const persegiLuas = new PersegiLuas(
-  "hasilPersegiLuas",
-  "hitungPersegiLuas",
-  helper
-);
-persegiLuas.hide();
-
-const persegiKeliling = new PersegiKeliling(
-  "hasilPersegiKeliling",
-  "hitungPersegiKeliling",
-  helper
-);
-persegiKeliling.hide();
-
-const btnTypePersegi = new ButtonType("btnTypePersegi");
-btnTypePersegi.activate();
 
